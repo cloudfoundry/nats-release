a24279e8726d118ecf441066b9e53e5fe092d66d
Aidan Obley <aobley@vmware.com>
2020-04-03 13:57:55 -0700

    fix: nats jobs can communicate to all other nats servers
    We added a matching bosh-dns-alias for each individual instance of the
    nats server. We can guarantee that they talk to every other server
    instead of only talking to half of the servers.
    
    [#172137909](https://www.pivotaltracker.com/story/show/172137909)
    
    Co-authored-by: Amelia Downs <adowns@vmware.com>

--- spec/nats/nats_config_spec.rb
+++ spec/nats/nats_config_spec.rb
@@ -17,14 +17,14 @@ module Bosh::Template::Test
             'port' => 4222,
             'cluster_port' => 4223,
             'authorization_timeout' => 15,
-            'machines' => ['192.168.1.1', '192.168.1.2'],
+            'machines' => nil,
             'debug' => false,
             'trace' => false,
             'monitor_port' => 0,
             'prof_port' => 0,
             'internal' => {
               'tls' => {
-                'enabled' => false,
+                'enabled' => true,
                 'ca' => 'internal-tls-ca',
                 'certificate' => 'internal-tls-cert',
                 'private_key' => 'internal-tls-key'
@@ -38,14 +38,19 @@ module Bosh::Template::Test
         [
           Link.new(
             name: 'nats',
-            instances: [LinkInstance.new()],
+            instances: [
+              LinkInstance.new(id: 'meowmeowmeow'),
+              LinkInstance.new(id: 'a-b-c-d')
+            ],
             properties: {
-              'user' => 'my-user',
-              'password' => 'my-password',
-              'hostname' => 'my-host',
-              'port' => 4222,
-              'cluster_port' => 4223,
-              'monitor_port' => 0
+              'nats' => {
+                'user' => 'my-user',
+                'password' => 'my-password',
+                'hostname' => 'my-host',
+                'port' => 4222,
+                'cluster_port' => 4223,
+                'monitor_port' => 0
+              }
             }
           )
         ]
@@ -93,15 +98,95 @@ cluster \{
   \}
 
   
+  tls \{
+    ca_file: "/var/vcap/jobs/nats/config/internal_tls/ca.pem"
+    cert_file: "/var/vcap/jobs/nats/config/internal_tls/certificate.pem"
+    key_file: "/var/vcap/jobs/nats/config/internal_tls/private_key.pem"
+    cipher_suites: \[
+      "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+      "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
+    \]
+    curve_preferences: \[
+      "CurveP384"
+    \]
+    timeout: 5 # seconds
+    verify: true
+  \}
+  
 
   routes = \[
     
-    nats-route://my-user:my-password@my-host:4223
+    nats-route://my-user:my-password@meowmeowmeow.my-host:4223
+    
+    nats-route://my-user:my-password@a-b-c-d.my-host:4223
     
   \]
 \}
 }
             expect(rendered_template).to include(expected_template)
+          end
+          describe 'nats machine ips are provided' do
+            before do
+              merged_manifest_properties['nats']['machines'] = ['192.0.0.1', '198.5.4.3']
+            end
+
+            it 'renders the template with the provided manifest properties' do
+              rendered_template = template.render(merged_manifest_properties, consumes: links, spec: spec)
+expected_template =  %{
+net: "10.0.0.1"
+port: 4222
+prof_port: 0
+monitor_port: 0
+
+debug: false
+trace: false
+logtime: true
+
+authorization \{
+  user: "my-user"
+  password: "my-password"
+  timeout: 15
+\}
+
+cluster \{
+  host: "10.0.0.1"
+  port: 4223
+
+  authorization \{
+    user: "my-user"
+    password: "my-password"
+    timeout: 15
+  \}
+
+  
+  tls \{
+    ca_file: "/var/vcap/jobs/nats/config/internal_tls/ca.pem"
+    cert_file: "/var/vcap/jobs/nats/config/internal_tls/certificate.pem"
+    key_file: "/var/vcap/jobs/nats/config/internal_tls/private_key.pem"
+    cipher_suites: \[
+      "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+      "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
+    \]
+    curve_preferences: \[
+      "CurveP384"
+    \]
+    timeout: 5 # seconds
+    verify: true
+  \}
+  
+
+  routes = \[
+    
+    nats-route://my-user:my-password@192.0.0.1:4223
+    
+    nats-route://my-user:my-password@198.5.4.3:4223
+    
+  \]
+\}
+}
+
+              expect(rendered_template).to include(expected_template)
+            end
           end
         end
       end
