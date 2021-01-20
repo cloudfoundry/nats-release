cbc536e145172f226a0f29367c7fb77b561eba1a
Andrew Edgar <aedgar@ca.ibm.com>
2021-01-20 08:08:35 -0600

    Add tests for new nats machines and fix all the old tests

--- spec/nats-tls/nats_tls_config_spec.rb
+++ spec/nats-tls/nats_tls_config_spec.rb
@@ -84,6 +84,7 @@ net: "10.0.0.1"
 port: 4222
 prof_port: 0
 http: "0.0.0.0:0"
+write_deadline: "2s"
 
 debug: false
 trace: false
@@ -161,6 +162,7 @@ net: "10.0.0.1"
 port: 4222
 prof_port: 0
 http: "0.0.0.0:0"
+write_deadline: "2s"
 
 debug: false
 trace: false
@@ -220,6 +222,91 @@ cluster \{
     nats-route://my-user:my-password@192.0.0.1:4223
     
     nats-route://my-user:my-password@198.5.4.3:4223
+    
+  \]
+\}
+}
+
+              expect(rendered_template).to include(expected_template)
+            end
+          end
+          describe 'nats machine ips and nontls_cluster_port are provided' do
+            before do
+              merged_manifest_properties['nats']['machines'] = ['192.0.0.1', '198.5.4.3']
+              merged_manifest_properties['nats']['nontls_cluster_port'] = '4225'
+            end
+
+            it 'renders the template with the provided manifest properties' do
+              rendered_template = template.render(merged_manifest_properties, consumes: links, spec: spec)
+expected_template =  %{
+net: "10.0.0.1"
+port: 4222
+prof_port: 0
+http: "0.0.0.0:0"
+write_deadline: "2s"
+
+debug: false
+trace: false
+logtime: true
+
+authorization \{
+  user: "my-user"
+  password: "my-password"
+  timeout: 15
+\}
+
+tls \{
+  ca_file: "/var/vcap/jobs/nats-tls/config/external_tls/ca.pem"
+  cert_file: "/var/vcap/jobs/nats-tls/config/external_tls/certificate.pem"
+  key_file: "/var/vcap/jobs/nats-tls/config/external_tls/private_key.pem"
+  cipher_suites: \[
+    "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+    "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
+  \]
+  curve_preferences: \[
+    "CurveP384"
+  \]
+  timeout: 5 # seconds
+  verify: true
+\}
+
+cluster \{
+  no_advertise: false
+  host: "10.0.0.1"
+  port: 4223
+
+  authorization \{
+    user: "my-user"
+    password: "my-password"
+    timeout: 15
+  \}
+
+  
+  tls \{
+    ca_file: "/var/vcap/jobs/nats-tls/config/internal_tls/ca.pem"
+    cert_file: "/var/vcap/jobs/nats-tls/config/internal_tls/certificate.pem"
+    key_file: "/var/vcap/jobs/nats-tls/config/internal_tls/private_key.pem"
+    cipher_suites: \[
+      "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+      "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
+    \]
+    curve_preferences: \[
+      "CurveP384"
+    \]
+    timeout: 5 # seconds
+    verify: true
+  \}
+  
+
+  routes = \[
+    
+    nats-route://my-user:my-password@192.0.0.1:4223
+    
+    nats-route://my-user:my-password@198.5.4.3:4223
+    
+    nats-route://my-user:my-password@192.0.0.1:4225
+    
+    nats-route://my-user:my-password@198.5.4.3:4225
     
   \]
 \}
--- spec/nats/nats_config_spec.rb
+++ spec/nats/nats_config_spec.rb
@@ -77,6 +77,7 @@ net: "10.0.0.1"
 port: 4222
 prof_port: 0
 http: "0.0.0.0:0"
+write_deadline: "2s"
 
 debug: false
 trace: false
@@ -139,6 +140,7 @@ net: "10.0.0.1"
 port: 4222
 prof_port: 0
 http: "0.0.0.0:0"
+write_deadline: "2s"
 
 debug: false
 trace: false
@@ -183,6 +185,76 @@ cluster \{
     nats-route://my-user:my-password@192.0.0.1:4223
     
     nats-route://my-user:my-password@198.5.4.3:4223
+    
+  \]
+\}
+}
+
+              expect(rendered_template).to include(expected_template)
+            end
+          end
+          describe 'nats machine ips and tls_cluster_port are provided' do
+            before do
+              merged_manifest_properties['nats']['machines'] = ['192.0.0.1', '198.5.4.3']
+              merged_manifest_properties['nats']['tls_cluster_port'] = '4225'
+            end
+
+            it 'renders the template with the provided manifest properties' do
+              rendered_template = template.render(merged_manifest_properties, consumes: links, spec: spec)
+expected_template =  %{
+net: "10.0.0.1"
+port: 4222
+prof_port: 0
+http: "0.0.0.0:0"
+write_deadline: "2s"
+
+debug: false
+trace: false
+logtime: true
+
+authorization \{
+  user: "my-user"
+  password: "my-password"
+  timeout: 15
+\}
+
+cluster \{
+  no_advertise: true
+  host: "10.0.0.1"
+  port: 4223
+
+  authorization \{
+    user: "my-user"
+    password: "my-password"
+    timeout: 15
+  \}
+
+  
+  tls \{
+    ca_file: "/var/vcap/jobs/nats/config/internal_tls/ca.pem"
+    cert_file: "/var/vcap/jobs/nats/config/internal_tls/certificate.pem"
+    key_file: "/var/vcap/jobs/nats/config/internal_tls/private_key.pem"
+    cipher_suites: \[
+      "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+      "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
+    \]
+    curve_preferences: \[
+      "CurveP384"
+    \]
+    timeout: 5 # seconds
+    verify: true
+  \}
+  
+
+  routes = \[
+    
+    nats-route://my-user:my-password@192.0.0.1:4223
+    
+    nats-route://my-user:my-password@198.5.4.3:4223
+    
+    nats-route://my-user:my-password@192.0.0.1:4225
+    
+    nats-route://my-user:my-password@198.5.4.3:4225
     
   \]
 \}
