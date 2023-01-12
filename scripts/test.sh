34c6881f0a6d8f7ceb717d7da4bd0b4321a5e2a8
Amelia Downs <adowns@vmware.com>
2023-01-12 17:07:51 +0000

    add test file for nats-v2-migration tests

---
+++ scripts/test.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+specificied_package="${1}"
+
+set -e -u
+
+go version # so we see the version tested in CI
+
+SCRIPT_PATH="$(cd "$(dirname "${0}")" && pwd)"
+
+cd "${SCRIPT_PATH}/.."
+
+declare -a packages=(
+  "src/code.cloudfoundry.org/nats-v2-migrate"
+)
+
+install_ginkgo() {
+  if ! [ $(type -P "ginkgo") ]; then
+    go install -mod=mod github.com/onsi/ginkgo/ginkgo@v1
+  fi
+}
+
+test_package() {
+  local package=$1
+  if [ -z "${package}" ]; then
+    return 0
+  fi
+  shift
+  pushd "${package}" &>/dev/null
+  ginkgo -r --race -randomizeAllSpecs -randomizeSuites -failFast \
+      -ldflags="extldflags=-WL,--allow-multiple-definition" \
+       "${@}";
+  rc=$?
+  popd &>/dev/null
+  return "${rc}"
+}
+
+install_ginkgo
+
+for dir in "${packages[@]}"; do
+    test_package "${dir}"
+done
+
